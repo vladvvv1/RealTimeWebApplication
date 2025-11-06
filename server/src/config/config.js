64d3c43a529773db2035c0e6cc9420a2939d6de7
D3f4ult_69 <vladvengr949@gmail.com>
2025-11-06 16:58:42 +0300

    .env configuration

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -8,6 +8,9 @@ export const PORT = process.env.PORT;
 export const MONGO_URL = process.env.MONGO_URL;
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
+export const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
+export const JWT_EXPIRATION = process.env.JWT_EXPIRATION;
+export const JWT_REFRESH_EXPIRATION = process.env.JWT_REFRESH_EXPIRATION;
 export const REDIS_URL = process.env.REDIS_URL;
 export const logDirectory = process.env.LOG_DIR;
 
